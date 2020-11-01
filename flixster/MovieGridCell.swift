613e3e75867efdfb9d50bbcc67bbf1f59a5efe1c
GL62 <40773055+GL62@users.noreply.github.com>
2020-11-01 05:50:42 -0500

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Guy Lingani on 10/31/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
